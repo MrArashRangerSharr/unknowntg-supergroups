194f119055f118e256ddf4c1da144a0b36eb3173
unknowntg <arashnomiri@gmail.com>
2016-05-02 02:11:57 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -225,7 +225,7 @@ function show_group_settingsmod(msg, target)
 		end
 	end
   local settings = data[tostring(target)]['settings']
-  local text = "Group settings:\nLock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nLock group leave : "..leave_ban.."\nflood sensitivity : "..NUM_MSG_MAX.."\nBot protection : "..bots_protection.."\nLock links : "..settings.lock_link.."\nLock RTL: "..settings.lock_rtl.."\nLock sticker: "..settings.lock_sticker.."\nPublic: "..settings.public
+  local text = "⚙Group settings:⚙\n______________________\n»Lock group name : "..settings.lock_name.."\»nLock group photo : "..settings.lock_photo.."\n»Lock group member : "..settings.lock_member.."\n»Lock group leave : "..leave_ban.."\n»flood sensitivity : "..NUM_MSG_MAX.."\n»Bot protection : "..bots_protection.."\n»Lock links : "..settings.lock_link.."\n»Lock RTL: "..settings.lock_rtl.."\n»Lock sticker: "..settings.lock_sticker.."\n»Public: "..settings.public.."\n_____________________\nGroup model:SuperGroup\nBot version:1"
   return text
 end
 
